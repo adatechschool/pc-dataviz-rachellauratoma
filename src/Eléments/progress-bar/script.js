789787011b09b336778816b6a0abcf321ced09a7
Rachel <rachel.bouhier@outlook.com>
2021-03-22 16:52:36 +0100

    [Feat][popup/] Amélioration style CSS

--- src/Eléments/progress-bar/script.js
+++ src/Eléments/progress-bar/script.js
@@ -7,13 +7,20 @@ function addPercentage(htmlElement, limit, offset) {
     let formattedNewValue = w.toString() + '%'
     document.getElementById('progress-bar-text').innerHTML = formattedNewValue
     htmlElement.style.width = formattedNewValue
+    // displayPopup()
 }
-function getElementWidth (id) {
+function getElementWidth(id) {
     return parseInt(document.getElementById(id).style.width)
 }
 
 function updateProgressBar(limit, progressBarId) {
-    setInterval(addPercentage, 2000,document.getElementById(progressBarId),limit,1)
+    setInterval(addPercentage, 20,document.getElementById(progressBarId),limit,1)
 }
-
+// function displayPopup() {
+//     if (getElementWidth('progress') == 99){
+//         console.log('Response');
+//         window.location.href = "http://127.0.0.1:5500/src/Eléments/popup/popup.html";
+//     } 
+// }
 updateProgressBar(99,'progress');
+
