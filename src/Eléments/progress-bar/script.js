d89c68d63c088e2cc5f9a840e54b6dd2de97599f
LauraGonzdc <lgonzdc@gmail.com>
2021-03-23 11:53:06 +0100

    [add][progress-bar.html] link progress bar/popup

--- src/Eléments/progress-bar/script.js
+++ src/Eléments/progress-bar/script.js
@@ -7,7 +7,7 @@ function addPercentage(htmlElement, limit, offset) {
     let formattedNewValue = w.toString() + '%'
     document.getElementById('progress-bar-text').innerHTML = formattedNewValue
     htmlElement.style.width = formattedNewValue
-    // displayPopup()
+    displayPopup()
 }
 function getElementWidth(id) {
     return parseInt(document.getElementById(id).style.width)
@@ -16,11 +16,11 @@ function getElementWidth(id) {
 function updateProgressBar(limit, progressBarId) {
     setInterval(addPercentage, 20,document.getElementById(progressBarId),limit,1)
 }
-// function displayPopup() {
-//     if (getElementWidth('progress') == 99){
-//         console.log('Response');
-//         window.location.href = "http://127.0.0.1:5500/src/Eléments/popup/popup.html";
-//     } 
-// }
+function displayPopup() {
+  if (getElementWidth('progress') == 99){
+         console.log('Response');
+         window.location.href = "http://127.0.0.1:5500/src/Eléments/popup/popup.html";
+    } 
+}
 updateProgressBar(99,'progress');
 
